68e7338eceefc9c199fd4ccc3d5e39da9969579b
达斯特兰斯 <kk580kk@gmail.com>
2016-01-05 14:05:27 +0800

    2016年1月4日 重构代码到spring-boot

--- src/main/java/com/baosight/wxscreen/Application.java
+++ src/main/java/com/baosight/wxscreen/Application.java
@@ -3,6 +3,7 @@ package com.baosight.wxscreen;
 /**
  * Created by GeekSeeker on 1/4/16.
  */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/baosight/wxscreen/WallController.java
+++ src/main/java/com/baosight/wxscreen/WallController.java
@@ -13,15 +13,44 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/wall")
 public class WallController {
     @RequestMapping(value = "/api/message/sync", produces = "text/json;charset=UTF-8", method = RequestMethod.POST)
-    public ModelAndView message(@RequestParam(value = "last_id") String last_id, @RequestParam(value = "tag_id") String tag_id, @RequestParam(value = "state") String state) {
+    public ModelAndView message(@RequestParam(value = "last_id", required = false) String last_id,
+                                @RequestParam(value = "tag_id", required = false) String tag_id,
+                                @RequestParam(value = "state") String state) {
         ModelAndView modelAndView = new ModelAndView("message");
         if ("144192289918982554".equals(last_id)) {
-            modelAndView = new ModelAndView("view/messageEmpty.vm");
+            modelAndView = new ModelAndView("messageEmpty");
 
         }
         if ("app".equals(state)) {
-            modelAndView = new ModelAndView("view/message.vm");
+            modelAndView = new ModelAndView("message");
         }
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/api/user/sync", produces = "text/json;charset=UTF-8", method = RequestMethod.POST)
+    public ModelAndView user(@RequestParam(value = "state") String state,
+                             @RequestParam(value = "last_id", required = false) String last_id,
+                             @RequestParam(value = "last_whitelist_id", required = false) String last_whitelist_id) {
+        ModelAndView modelAndView = new ModelAndView("sync");
+        if ("message".equals(state)) {
+            if ("144192289919006300".equals(last_id)) {
+                modelAndView = new ModelAndView("sync2");
+            }
+        }
+        if ("app".equals(state)) {
+            if ("144192289919006300".equals(last_id)) {
+                modelAndView = new ModelAndView("sync2");
+            }
+        }
+        if ("lottery".equals(state)) {
+            modelAndView = new ModelAndView("lottery");
+        }
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/api/user/notice/get_latest_notice", produces = "text/json;charset=UTF-8", method = RequestMethod.POST)
+    public ModelAndView message() {
+        ModelAndView modelAndView = new ModelAndView("notice");
         return modelAndView;
     }
 }
